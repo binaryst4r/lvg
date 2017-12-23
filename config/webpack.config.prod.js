ff4e5c4b59efb5647f0519662f28e64ed3d94970
Larry Gust <larry.v.gust@gmail.com>
2017-12-23 16:13:23 -0600

    pmg

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -148,31 +148,9 @@ module.exports = {
             },
           },
           {
-            test: /\.scss$/,
-            use: [
-              require.resolve('style-loader'),
-              {
-                loader: require.resolve('sass-loader'),
-                options: {
-                  // Necessary for external CSS imports to work
-                  // https://github.com/facebookincubator/create-react-app/issues/2677
-                  ident: 'postcss',
-                  plugins: () => [
-                    require('postcss-flexbugs-fixes'),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }),
-                  ],
-                },
-              },
-            ],
-          },
+            test   : /\.scss$/,
+            loaders: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']}
+          {
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
