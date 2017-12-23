8113fdb63cd7965f28fae0f42a8f91027a2f419a
Larry Gust <larry.v.gust@gmail.com>
2017-12-23 15:34:50 -0600

    rebuild

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -154,8 +154,36 @@ module.exports = {
             },
           },
           {
-            test:    /\.scss?$/,
-            loader: ExtractTextPlugin.extract(['sass-loader'])
+            test: /\.scss$/,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                },
+              },
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+            ],
           },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
@@ -216,7 +244,7 @@ module.exports = {
           },
           {
             loader: require.resolve('file-loader'),
-            exclude: [/\.js$/, /\.html$/, /\.json$/],
+            exclude: [/\.js$/, /\.html$/, /\.json$/, /\.scss$/,/\.sass$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
